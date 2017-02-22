dae2f9185c04f03edeffe57373b4009f97fd2aeb
Zac King <ZacKingx@gmail.com>
2017-02-22 10:50:41 -0600

    node Teleport and adjustments to scene
    -adjustment to spot the diff game
    -scene lighting
    -added a look at cam script
    -and all the stuff for Teleporting nodes

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Makes object look at the main Camera
+/// </summary>
+ 
+public class LookAtCamera : MonoBehaviour
+{
+    Transform m_view;
+
+    private void Start()
+    {
+        m_view = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(m_view);
+    }
+}
