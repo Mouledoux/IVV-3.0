8553de6256265c8e0fce18b0feefaf14300140fe
Zac King <ZacKingx@gmail.com>
2017-03-10 16:23:35 -0600

    Adding Web Corner

---
+++ Assets/OpenURL.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public void OpenURLLink()
+    {
+        Application.ExternalEval("window.open('https://www.tantrumlab.com/');");
+    }
+}
